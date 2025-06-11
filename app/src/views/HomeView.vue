b91eac9712324e171519bf79bc6d367c5c09d07e
Jonas Moriá <moriah.tecnologia.dev@gmail.com>
2025-06-11 16:30:13 -0300

    config: cleaning vue3 + installing tailwind css

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <NavBarComponent/>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavBarComponent from '@/components/Home/NavBarComponent.vue';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'HomeView',
+
+    components: {
+        NavBarComponent
+    }
 }
 </script>
