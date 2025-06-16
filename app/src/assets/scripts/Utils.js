6aec9b4c230dd08f32c032d355f6a2d32aa86efa
Jonas Moriá <moriah.tecnologia.dev@gmail.com>
2025-06-15 22:52:14 -0300

    feat: creating a dinamic navbar to user logged

--- app/src/assets/scripts/Utils.js
+++ app/src/assets/scripts/Utils.js
@@ -2,54 +2,63 @@ import router from "@/router"
 import localBase from "./LocalBase"
 
 const Utils = {
-  maskCpf: (cpf) => {
-    if (!cpf) return ''
+    maskCpf: (cpf) => {
+        if (!cpf) return ''
 
-    let value = cpf.replace(/\D/g, '')
+        let value = cpf.replace(/\D/g, '')
 
-    value = value.slice(0, 11)
+        value = value.slice(0, 11)
 
-    value = value.replace(/(\d{3})(\d)/, '$1.$2')
-    value = value.replace(/(\d{3})(\d)/, '$1.$2')
-    value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2')
+        value = value.replace(/(\d{3})(\d)/, '$1.$2')
+        value = value.replace(/(\d{3})(\d)/, '$1.$2')
+        value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2')
 
-    return value
-  },
+        return value
+    },
 
-  maskCnpj: (cnpj) => {
-    if (!cnpj) return ''
+    maskCnpj: (cnpj) => {
+        if (!cnpj) return ''
 
-    let value = cnpj.replace(/\D/g, '')
+        let value = cnpj.replace(/\D/g, '')
 
-    value = value.slice(0, 14)
+        value = value.slice(0, 14)
 
-    value = value.replace(/^(\d{2})(\d)/, '$1.$2')
-    value = value.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3')
-    value = value.replace(/\.(\d{3})(\d)/, '.$1/$2')
-    value = value.replace(/(\d{4})(\d)/, '$1-$2')
+        value = value.replace(/^(\d{2})(\d)/, '$1.$2')
+        value = value.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3')
+        value = value.replace(/\.(\d{3})(\d)/, '.$1/$2')
+        value = value.replace(/(\d{4})(\d)/, '$1-$2')
 
-    return value
-  },
+        return value
+    },
 
-  checkUserAlreadyLogged: () => {
-      const userSession = JSON.parse(
-          localBase.select(localBase.keys.login.user)
-      );
-      const enterpriseSession = JSON.parse(
-          localBase.select(localBase.keys.login.enteprise)
-      );
+    checkUserAlreadyLogged: () => {
+        const userSession = JSON.parse(
+            localBase.select(localBase.keys.login.user)
+        );
+        const enterpriseSession = JSON.parse(
+            localBase.select(localBase.keys.login.enteprise)
+        );
 
-      if (userSession) {
-          return router.push({
-              path: '/usuario/home'
-          });
-      }
-      if (enterpriseSession) {
-          return router.push({
-              path: '/empresa/home'
-          });
-      }
-  }
+        if (userSession) {
+            return router.push({
+                path: '/usuario/home'
+            });
+        }
+        if (enterpriseSession) {
+            return router.push({
+                path: '/empresa/home'
+            });
+        }
+    },
+
+    destroySession: () => {
+        localBase.remove(localBase.keys.login.user);
+        localBase.remove(localBase.keys.login.enteprise);
+
+        return router.push({
+            path: '/login'
+        });
+    }
 }
 
 export default Utils
--- app/src/components/Home/NavBarComponent.vue
+++ app/src/components/Home/NavBarComponent.vue
@@ -18,40 +18,131 @@
         </div>
 
         <div class="flex-row hidden md:block pt-2">
-            <router-link to="/login" class="text-green-700 border border-green-700 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2">Entrar</router-link>
-            <router-link to="/cadastro" class="text-white border border-green-700 bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2">Cadastrar</router-link>
-        </div>
+            <div v-if="!isLogged">
+                <router-link to="/login" class="text-green-700 border border-green-700 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2">Entrar</router-link>
+                <router-link to="/cadastro" class="text-white border border-green-700 bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2">Cadastrar</router-link>
+            </div>
+            <div v-if="isLogged" class="relative flex flex-row items-center cursor-pointer" @click="toggleDropdown()">
+                <img class="w-6 h-6 p-1 rounded-full ring-2 ring-green-700" :src="sessionData.perfilImageSrc" alt="perfil_logo">
+                <span class="text-sm font-semibold ml-2">Olá, {{ sessionData.name }}!</span>
 
+                <!-- DROPDOWN MENU -->
+                <div v-if="isDropdownOpen" class="absolute right-0 top-full mt-2 w-40 bg-white border rounded-md shadow-lg z-50">
+                    <router-link :to="sessionData.redirectHomeTo" class="block px-4 py-2 text-sm hover:bg-gray-100">Home</router-link>
+                    <router-link :to="sessionData.redirectHomeTo" class="block px-4 py-2 text-sm hover:bg-gray-100">Perfil</router-link>
+                    <button @click="logout()" class="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100">Sair</button>
+                </div>
+            </div>
+
+        </div>
         <div class="block md:hidden mt-1" @click="manageSideNav()">
             <img src="@/assets/icons/menu.svg" alt="menu_login" class="h-7">
         </div>
     </div>
 </nav>
+
 <aside class="fixed top-18 left-0 w-full h-[calc(100vh-4rem)] bg-gray-50 z-10 transition-transform duration-300 ease-in-out" :class="isSideNavOpen ? 'translate-x-0' : 'translate-x-full'">
     <div class="p-3 text-center w-full">
         <img src="@/assets/icons/close.svg" alt="close_icon" class="h-8" @click="manageSideNav()">
     </div>
     <div class="flex flex-row w-full justify-center content-center">
-        <router-link to="/login" class="text-green-700 border border-green-700 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2">Entrar</router-link>
-        <router-link to="/cadastro" class="text-white border border-green-700 bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2">Cadastrar</router-link>
+        <div v-if="!isLogged">
+            <router-link to="/login" class="text-green-700 border border-green-700 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2">Entrar</router-link>
+            <router-link to="/cadastro" class="text-white border border-green-700 bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 text-center me-2">Cadastrar</router-link>
+        </div>
+        <div v-if="isLogged">
+            <div class="flex flex-row">
+                <img class="w-6 h-6 p-1 rounded-full ring-2 ring-green-700" :src="sessionData.perfilImageSrc" alt="perfil_logo">
+                <span class="text-sm font-semibold ml-2 mt-1">Olá, {{ sessionData.name }}!</span>
+            </div>
+            <hr class="mt-5">
+            <div class="w-full mt-3 text-center">
+                <router-link :to="sessionData.redirectHomeTo" class="block px-4 py-2 text-sm hover:bg-gray-100">Home</router-link>
+                <router-link :to="sessionData.redirectHomeTo" class="block px-4 py-2 text-sm hover:bg-gray-100">Perfil</router-link>
+                <button @click="logout()" class="block w-full px-4 py-2 text-sm text-red-600 hover:bg-gray-100">Sair</button>
+            </div>
+        </div>
     </div>
 </aside>
 </template>
 
 <script>
+import localBase from '@/assets/scripts/LocalBase';
+import Utils from '@/assets/scripts/Utils';
+
 export default {
     name: 'NavBarComponent',
 
     data() {
         return {
             isSideNavOpen: false,
+            isLogged: false,
+            isDropdownOpen: false,
+            sessionData: {
+                name: '',
+                type: 0,
+                perfilImageSrc: null,
+                redirectHomeTo: ''
+            }
         }
     },
 
     methods: {
         manageSideNav: function () {
             this.isSideNavOpen = !this.isSideNavOpen;
+        },
+        toggleDropdown() {
+            this.isDropdownOpen = !this.isDropdownOpen;
+        },
+        logout() {
+            Utils.destroySession();
+        },
+        handleClickOutside(event) {
+            if (!this.$el.contains(event.target)) {
+                this.isDropdownOpen = false;
+            }
         }
-    }
+    },
+
+    created() {
+        let sessionInfo = null;
+
+        const userData = localBase.select(localBase.keys.login.user);
+        const enterpriseData = localBase.select(localBase.keys.login.enteprise);
+
+        try {
+            if (userData) {
+                sessionInfo = JSON.parse(userData);
+            } else if (enterpriseData) {
+                sessionInfo = JSON.parse(enterpriseData);
+            }
+        } catch (e) {
+            this.isLogged = false;
+            return;
+        }
+
+        if (sessionInfo && sessionInfo.token) {
+            this.isLogged = true;
+            this.sessionData.name = sessionInfo.user.name;
+            this.sessionData.type = sessionInfo.user.type;
+
+            switch (sessionInfo.user.type) {
+                case 1:
+                    this.sessionData.perfilImageSrc = new URL('@/assets/icons/person.svg',
+                        import.meta.url).href;
+                    this.sessionData.redirectHomeTo = '/usuario/home';
+                    break;
+                case 2:
+                    this.sessionData.perfilImageSrc = new URL('@/assets/icons/company.svg',
+                        import.meta.url).href;
+                    this.sessionData.redirectHomeTo = '/empresa/home';
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            this.isLogged = false;
+        }
+    },
 }
 </script>
