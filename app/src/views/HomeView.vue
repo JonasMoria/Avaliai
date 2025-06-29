b6717b6d747b5b7f11a0ccfb81c52f697d1ee05a
Jonas Moriá <moriah.tecnologia.dev@gmail.com>
2025-06-28 23:21:18 -0300

    feat: wave effect home title

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,10 +3,23 @@
     <NavBarComponent />
 
     <section class="flex-grow">
-        <div class="w-full bg-gray-100 h-48 p-4 flex flex-col justify-center items-center">
-            <h3 class="text-2xl font-bold text-gray-600 text-center">Descubra o melhor do mercado com o Avalia Ai!</h3>
-            <h6 class="mt-2 text-sm text-gray-700 text-center">Encontre empresas incríveis, serviços de qualidade e confira avaliações reais do público — tudo ao seu alcance!</h6>
-        </div>
+        <section class="flex-grow">
+            <div class="relative bg-green-300">
+                <div class="h-56 p-4 flex flex-col justify-center items-center">
+                    <h3 class="text-3xl font-bold text-gray-600 text-center">
+                        Descubra o melhor do mercado com o Avalia Ai!
+                    </h3>
+                    <h6 class="mt-2 text-xs text-gray-700 text-center font-semibold">
+                        Encontre empresas incríveis, serviços de qualidade e confira avaliações reais do público — tudo ao seu alcance!
+                    </h6>
+                </div>
+                <div class="absolute bottom-0 left-0 w-full overflow-hidden leading-[0] h-20">
+                    <svg class="relative block w-full h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                        <path fill="#fff" d="M0,160L48,149.3C96,139,192,117,288,128C384,139,480,181,576,181.3C672,181,768,139,864,117.3C960,96,1056,96,1152,128C1248,160,1344,224,1392,256L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+                    </svg>
+                </div>
+            </div>
+        </section>
 
         <div class="w-full mt-4 p-4 flex flex-col justify-center items-center">
             <h3 class="text-2xl font-bold text-gray-600 text-center">Explore as funcionalidades que vão revolucionar sua busca!</h3>
